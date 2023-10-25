522a9afa430ccf3d741074c6b6c1f4bb16d8f6f3
Tobias Oliveira <tobiastomaz_oliveira@hotmail.com>
2023-10-25 18:49:37 -0300

    Migrations

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, "recipes/pages/home.html")
+    return render(request, "recipes/pages/home.html", context={
+        'recipes': [make_recipe() for _ in range(9)],
+    })
 
 
 def recipes(request, id):
-    return render(request, "recipes/pages/recipes-view.html")
+    return render(request, "recipes/pages/recipes-view.html", context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })

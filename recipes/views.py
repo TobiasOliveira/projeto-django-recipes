9910ad35e3cb1ab17fdd4016b95c04518e5d3a81
Tobias Oliveira <tobiastomaz_oliveira@hotmail.com>
2023-10-28 18:31:56 -0300

    Test Runner

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,10 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipes
 
 
 def home(request):
-    recipes = Recipes.objects.filter(is_published=True).order_by('-id')
-
-    return render(request, "recipes/pages/home.html", context={
-        'recipes': recipes,
-    })
-
-
-def category(request, category_id):
     recipes = Recipes.objects.filter(
-        category__id=category_id,
-        is_published=True
+        is_published=True,
     ).order_by('-id')
 
     return render(request, "recipes/pages/home.html", context={
@@ -22,8 +12,27 @@ def category(request, category_id):
     })
 
 
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipes.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
+    return render(request, "recipes/pages/home.html", context={
+        'recipes': recipes,
+    })
+
+
 def recipes(request, id):
+    recipes = get_object_or_404(
+        Recipes.objects.filter(
+            pk=id,
+            is_published=True,
+        )
+    )
     return render(request, "recipes/pages/recipes-view.html", context={
-        'recipe': make_recipe(),
+        'recipe': recipes,
         'is_detail_page': True,
     })

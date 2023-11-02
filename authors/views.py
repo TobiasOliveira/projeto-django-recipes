8ebcae1c904c7bd04592e922a2c2ce9b99858e3d
Tobias Oliveira <tobiastomaz_oliveira@hotmail.com>
2023-11-01 23:47:51 -0300

    Form creation

---
+++ authors/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from authors.forms import RegisterForm
+from django.http import Http404
+
+
+def register_view(request):
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
+
+    return render(request, 'authors/pages/register_view.html', context={
+        'form': form
+    })
+
+
+def register_created(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+
+    return redirect('authors:register')
